a203d15a18a45f83ed8546823a59da9d1c2777d3
sangmin <min62645@naver.com>
2024-10-17 15:57:08 +0900

    자바 코딩 테스트 자료

---
+++ exam07/Test05.java
@@ -0,0 +1,8 @@
+package exam07;
+
+// "ABC/n"을 표시
+public class Test05 {
+    public static void main(String[] args) {
+        System.out.println("\"ABC\\n\"");
+    }
+}
---
+++ exam14/CarExample.java
@@ -0,0 +1,12 @@
+package exam14;
+
+public class CarExample {
+    public static void main(String[] args) {
+        Car myCar = new Car();
+
+        myCar.setSpeed(-50);
+        System.out.println("현재 속도:" + myCar.gerSpeed());
+    }
+
+
+}
